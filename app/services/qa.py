5e8c54f3387b7140785b22f895be5fb7fecfffb9
Stanislaw <satach@o2.pl>
2025-08-23 23:54:29 +0200

    perf: add batch size and cross encoder

--- app/services/qa.py
+++ app/services/qa.py
@@ -4,13 +4,15 @@ from langchain.chains import RetrievalQA
 from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.vectorstores import Qdrant
 from qdrant_client import QdrantClient
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.rerank.base import CrossEncoderReranker
 
 from services.ollama_llm import OllamaLLM
 
 
 def init_qa_chain(qdrant_url: str, ollama_url: str) -> RetrievalQA:
     llm = OllamaLLM(ollama_url=ollama_url)
-    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2", batch_size = 32)
 
     parsed_url = urlparse(qdrant_url)
     host = parsed_url.hostname or "localhost"
@@ -20,4 +22,10 @@ def init_qa_chain(qdrant_url: str, ollama_url: str) -> RetrievalQA:
 
     vectordb = Qdrant(client=client, collection_name="finance_docs", embeddings=embeddings)
     retriever = vectordb.as_retriever(search_kwargs={"k": 4})
-    return RetrievalQA.from_chain_type(llm=llm, retriever=retriever, return_source_documents=True)
+    reranker = CrossEncoderReranker(model_name="cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=reranker,
+        base_retriever=retriever,
+    )
+    return RetrievalQA.from_chain_type(llm=llm, retriever=compression_retriever , return_source_documents=True)
